d1fa8e1ee35ce3b4b87394dba8d1483bc03daaaf
ptspillane <pspillane@515tech.com>
2023-09-25 17:49:46 -0400

    Update: missing fields
    forgot to transfer user_id fields in todolist model

--- models/todolist.py
+++ models/todolist.py
@@ -5,5 +5,8 @@ class TodoListModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), unique=True, nullable=False)
+ 
+    users = db.relationship("UserModel", back_populates="todolist")
     tasks = db.relationship("TaskModel", back_populates="todolist", lazy="dynamic", cascade="all, delete")
     categories = db.relationship("CategoryModel", back_populates="todolist", lazy="dynamic")
